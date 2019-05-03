e75b51fe5d50615243e0d8b5d4c8aa4136c830a0
Stan Hughes <hughes.stan@gmail.com>
2019-05-03 14:50:01 -0400

    Added 'welcome' string

--- App.js
+++ App.js
@@ -16,12 +16,14 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const welcome = 'Welcome to React Native! Now with ESLint';
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>{welcome}</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
